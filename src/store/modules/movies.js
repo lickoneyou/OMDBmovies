b4b84488f426324660c340eae9e2d384056d46d3
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-12-27 20:10:35 +0300

    completed movies store, fix no-unused-vars linter

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,15 +1,54 @@
 import axios from "@/plugins/axios";
+import IDs from "@/store/mock/imdb_top250";
+import mutation from "../mutation";
+
+const serializeResponse = (movies) =>
+  movies.reduce((acc, movie) => {
+    acc[movie.imdbID] = movie;
+    return acc;
+  }, {});
+
+const { MOVIES } = mutation;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+    movies: {
+      [MOVIES](state, value) {
+        state.movies = value;
+      },
+    },
+  },
+  getters: {
+    slicedIDs({ top250IDs }) {
+      return (from, to) => top250IDs.slice(from, to);
+    },
+    currentPage({ currentPage }) {
+      return currentPage;
+    },
+    moviesPerPage({ moviesPerPage }) {
+      return moviesPerPage;
+    },
+  },
   mutations: {},
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const res = await axios.get("/?i=tt0111161");
-      console.log(res);
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { currentPage, moviesPerPage, slicedIDs } = getters;
+        const from = currentPage * moviesPerPage - moviesPerPage;
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicedIDs(from, to);
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+        const res = await Promise.all(requests);
+        const data = res.map((el) => el.data);
+        const movies = serializeResponse(data);
+        commit(MOVIES, movies);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
